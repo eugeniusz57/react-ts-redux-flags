7f9eb962e1ee9b98e64729553c71e400535db374
eugeniusz57 <zenakaveckij1@gmail.com>
2023-05-21 12:47:14 +0200

    create types

---
+++ src/types/countries.ts
@@ -0,0 +1,26 @@
+type Currency = {
+    symbol: string,
+    code: string,
+    name: string,
+}
+type Language = {
+    iso639_1: string,
+    iso639_2: string,
+    name: string,
+    nativeName: string,
+}
+
+export type Country = {
+    name: string,
+    nativeName: string,
+    flag: string,
+    flags: [{ png: string }, { svg: string }],
+    region: string,
+    subregion: string,
+    capital: string,
+    population: number,
+    topLevelDomain: string[],
+    borders: string[],
+    currencies: Currency[],
+    languages: Language[],
+}
